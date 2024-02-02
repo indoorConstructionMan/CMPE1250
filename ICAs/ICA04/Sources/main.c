29488fa4d4f0d171e0fae3e965f898427df82060
Aaron <indoorconstructionman@gmail.com>
2024-02-02 15:00:46 -0700

    pushed new code something something

--- ICAs/ICA04/Sources/main.c
+++ ICAs/ICA04/Sources/main.c
@@ -96,26 +96,3 @@ void Trap1() {
 /********************************************************************/
 // Interrupt Service Routines
 /***********************************/
-=======
-#include <hidef.h>      /* common defines and macros */
-#include "derivative.h"      /* derivative-specific definitions */
-
-
-
-
-
-
-void main(void) {
-  /* put your own code here */
-  
-
-
-	EnableInterrupts;
-
-
-  for(;;) {
-    _FEED_COP(); /* feeds the dog */
-  } /* loop forever */
-  /* please make sure that you never leave main */
-}
->>>>>>> 91ec8eb48609a6b83f3c98d1b872bddfc9cd5a48
