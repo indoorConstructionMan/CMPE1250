20570606a42a93b6fad81e4af1cca2cc13b115ef
Aaron <indoorconstructionman@gmail.com>
2024-02-19 00:28:08 -0700

    Added some comments, cleaned up with ifdef

--- ICAs/ICA06/Sources/main.c
+++ ICAs/ICA06/Sources/main.c
@@ -1,10 +1,10 @@
 /********************************************************************/
-// HC12 Program:  Lab Exam 1
+// HC12 Program:  ICA06 - PLL/BUS Speed
 // Processor:     MC9S12XDP512
-// Bus Speed:     40 MHz
+// Bus Speed:     **Assign BUS Speed 8/16/20/24/40 MHz (80 Doesn't work)
 // Author:        Aaron Arnason
 // Details:       A more detailed explanation of the program is entered here               
-// Date:          February 16, 2024
+// Date:          February 19, 2024
 // Revision History :
 //    ***Check Lib folder sw_led.h/sw_led.c for functions used
 //    
@@ -24,6 +24,8 @@
 /********************************************************************/
 //Defines
 /********************************************************************/
+//#define PART1
+#define PART2
 /********************************************************************/
 // Local Prototypes
 /********************************************************************/
@@ -56,20 +58,29 @@ void main(void)
 /********************************************************************/
   // main program loop
 // /********************************************************************/
-//   // Part 1
-//   for (;;) {
-//     OnOffDutyCycle(SWL_RED, blockingVariableOn, blockingVariableOff);
-//   }    
+  #ifdef PART1
+    // Part 1
+    for (;;) {
+      OnOffDutyCycle(SWL_RED, blockingVariableOn, blockingVariableOff);
+    }    
+  #endif
 
-  //Clock_Set20MHZ(); // sets clock to 20 mhz
-  //Clock_EnableOutput(ClockOutDiv4); // div1 = 20MHz, div2 = 10MHz, div3 = 6.67 MHz, div4 = 5Mhz
+  #ifdef PART2
+    // ClockEnableOutput divider doesn't change OnOffDutyCycle
+    // Duty cycle changes with Clock_SetXXMHZ()
+    //Clock_Set20MHZ(); // sets clock to 20 mhz
+    //Clock_EnableOutput(ClockOutDiv4); // div1 = 20MHz, div2 = 10MHz, div3 = 6.67 MHz, div4 = 5Mhz
+    
+    //Clock_Set24MHZ(); // sets clock to 24 mhz
+    //Clock_EnableOutput(ClockOutDiv2); // div1 = 24MHz, div2 = 12MHz, div3 = 8 MHz, div4 = 6 Mhz
 
-  Clock_Set24MHZ(); // sets clock to 24 mhz
-  Clock_EnableOutput(ClockOutDiv2); // div1 = 24MHz, div2 = 12MHz, div3 = 8 MHz, div4 = 6 Mhz
-  // Part 2
-  for (;;) {
-    OnOffDutyCycle(SWL_RED, blockingVariableOn, blockingVariableOff);
-  }        
+    Clock_Set40MHZ(); // sets clock to 40 mhz
+    Clock_EnableOutput(ClockOutDiv4); // div1 = 40MHz, div2 = 20MHz, div3 = 13.3 MHz, div4 = 10 Mhz
+    // Part 2
+    for (;;) {
+      OnOffDutyCycle(SWL_RED, blockingVariableOn, blockingVariableOff);
+    }     
+  #endif
 }
 
 /********************************************************************/
