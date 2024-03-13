438a2aa95dc6170f958c9467c609b1d599d92c23
Aaron <indoorconstructionman@gmail.com>
2024-03-13 09:47:35 -0600

    got sci working

--- ICAs/ICA08/Sources/main.c
+++ ICAs/ICA08/Sources/main.c
@@ -1,21 +1,95 @@
+/********************************************************************/
+// HC12 Program:  ICA08 - SCI library
+// Processor:     MC9S12XDP512
+// Bus Speed:     20 MHz
+// Author:        Aaron Arnason
+// Details:       This program uses interrupts to generate a 1 second delay
+// Date:          March 13, 2024
+// Revision History :
+//  each revision will have a date + desc. of changes
+
+
+
+/********************************************************************/
+// Library includes
+/********************************************************************/
 #include <hidef.h>      /* common defines and macros */
-#include "derivative.h"      /* derivative-specific definitions */
+#include "derivative.h" /* derivative-specific definitions */
+#include "sw_led.h"     /* For LED/Switch Functions */
+#include "pll_clock.h"  /* Set bus speed to 20Mhz */
+
+//Other system includes or your includes go here
+//#include <stdlib.h>
+//#include <stdio.h>
 
 
+/********************************************************************/
+//Defines
+/********************************************************************/
 
+/********************************************************************/
+// Local Prototypes
+/********************************************************************/
+// RTI interrupt prototype
+//interrupt VectorNumber_Vrti void Vrti_ISR(void);
 
+/********************************************************************/
+// Global Variables
+/********************************************************************/
 
+/********************************************************************/
+// Constants
+/********************************************************************/
 
-void main(void) {
-  /* put your own code here */
-  
+/********************************************************************/
+// Main Entry
+/********************************************************************/
+void main(void)
+{
+  //Any main local variables must be declared here
+  char data = 'A';
+  // main entry point
+  _DISABLE_COP();
+  // Set Bus Speed
+  Clock_Set20MHZ();
+  // Init the Switches/LEDs
+  SWL_Init();
+  //EnableInterrupts;
+	
+  // Set baud rate using 20 mhz bus
+  SCI0BD = 130;
 
+  // Enable TX/RX of SCI module
+  SCI0CR2 |= SCI0CR2_RE_MASK | SCI0CR2_TE_MASK;
 
-	EnableInterrupts;
+/********************************************************************/
+  // one-time initializations
+/********************************************************************/
 
+/********************************************************************/
+  // main program loop
+/********************************************************************/
+  for (;;) {
+    // If the buffer is empty, write a value: 
+    if (SCI0SR1_TDRE) {
+      SWL_ON(SWL_RED);
+      SCI0DRL = data;
+    }
 
-  for(;;) {
-    _FEED_COP(); /* feeds the dog */
-  } /* loop forever */
-  /* please make sure that you never leave main */
+    // if data available turn green led on
+    if (SCI0SR1_RDRF) {
+      SWL_ON(SWL_GREEN);
+      data = SCI0DRL;
+    }
+
+  }
 }
+
+/********************************************************************/
+// Functions
+/********************************************************************/
+
+/********************************************************************/
+// Interrupt Service Routines
+/********************************************************************/
+
