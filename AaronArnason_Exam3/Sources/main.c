6060638147603003f9fd4fb89fa32ba7d284ad9a
Aaron <indoorconstructionman@gmail.com>
2024-04-26 15:51:13 -0600

    Pushing the final exam. Finished question A, and B full marks.

--- AaronArnason_Exam3/Sources/main.c
+++ AaronArnason_Exam3/Sources/main.c
@@ -24,14 +24,15 @@
 #include "rti.h"
 
 //Other system includes or your includes go here
-//#include <stdlib.h>
-//#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 
 /********************************************************************/
 //Defines
 /********************************************************************/
-
+//#define questiona
+#define questionc
 /********************************************************************/
 // Local Prototypes
 /********************************************************************/
@@ -40,6 +41,17 @@
 // Global Variables
 /********************************************************************/
 int i;
+unsigned int minutes;
+unsigned int seconds;
+unsigned int tenthSeconds;
+
+char clock1[80];
+
+
+char userBuffer[500];
+char myChar;
+int charactersRX;
+int alphaCount;
 /********************************************************************/
 // Constants
 /********************************************************************/
@@ -65,7 +77,7 @@ void main(void)
   RTI_Init();
 
   // sci init
-  sci0_InitMath(Clock_GetBusSpeed(), 115200);
+  sci0_InitMath(Clock_GetBusSpeed(), 19200);
 
   //segs init
   Segs_Init();
@@ -79,14 +91,78 @@ void main(void)
   // one-time initializations
 /********************************************************************/
   i = 0;
+
+  #ifdef questiona
+  minutes = 0;
+  seconds = 0;
+  tenthSeconds = 0;
 /********************************************************************/
   // main program loop
 /********************************************************************/
+  Segs_Init();
   Segs_Clear();
-  lcd_Clear();
+
+
+  sci0_ClearScreen();
+  sci0_GoToXY(0,0);
+  sci0_txStr("Arnason");
 
   for (;;)
   {
+    RTI_Delay(100);
+    tenthSeconds++;
+    if (tenthSeconds == 10) {
+      tenthSeconds = 0;
+      seconds++;
+    }
+
+    if (seconds == 60) {
+      seconds = 0;
+      minutes++;
+    }
+
+    sprintf(clock1, "%02u:%02u:%0u", minutes, seconds, tenthSeconds);
+
+    sci0_txStrXY(12, 40, clock1);
+
+    Segs_16D(i, Segs_LineTop);
+
+    i++;
+    if (i == 9999) i = 0;
+  }               
+  #endif
+
+
+  #ifdef questionc
+  Segs_Clear();
+  charactersRX = 0;
+  lcd_DispControl(0,0);
+  alphaCount = 0;
+  for (;;) {
+
+    myChar = sci0_bread();
+    sci0_txByte(myChar);
+    charactersRX++;
+    Segs_16D(charactersRX, Segs_LineTop);
+    
+    Segs_8H(6, myChar);
+    if (myChar >= 65 && myChar <= 90 || myChar >= 97 && myChar <= 122) {
+      alphaCount++;
+      lcd_StringXY(0, 0,&myChar);
+      sprintf(userBuffer, "Alphas: %04u", alphaCount);
+      lcd_StringXY(0, 2, userBuffer);
+    }
+      
+  }
+  
+  #endif    
+}
+
+/********************************************************************/
+// Functions
+/********************************************************************/
+/**
+
     RTI_Delay(1000);
     SWL_TOG(SWL_RED);
 
@@ -95,13 +171,7 @@ void main(void)
     i++;
     lcd_Data(i);
     //lcd_String("Does this thing even work?");
-  }                   
-}
-
-/********************************************************************/
-// Functions
-/********************************************************************/
-
+*/
 /********************************************************************/
 // Interrupt Service Routines
 /********************************************************************/
